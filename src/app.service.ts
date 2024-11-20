dd8400c627ccb0a82fff097db2ac61a16720c109
as-express <aset16006@icloud.com>
2024-11-20 18:52:55 +0500

    fixed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello From Server Ubuntu!';
+    return 'Hello From Server Ubuntu! server';
   }
 }
